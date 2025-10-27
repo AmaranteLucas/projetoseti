120d4c5b1fab3f0bb4404dd71ea417fe383c65f9
AmaranteLucas <72353318+AmaranteLucas@users.noreply.github.com>
2025-10-27 12:47:55 -0300

    Adição do Portal do Aluno de Guapi

--- js/script.js
+++ js/script.js
@@ -57,8 +57,8 @@ function abrirLink(menu, valor) {
         'portalAluno': {
             'Saquarema': 'https://hml-portaldoaluno.siseducsaquarema.org.br/',
             'Arraial': 'https://hml-portaldoaluno.siseducarraial.org.br/',
-            'São Gonçalo': '-',
-            'Guapimirim': '-'
+            'Guapimirim': 'https://hml-portaldoaluno.educaguapi.org.br/',
+            'São Gonçalo': '-'
         }
     };
 
